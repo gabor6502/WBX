47fe6ff64c7829f88ef6f32171b162931f313088
gabor6502 <elan.gabor@gmail.com>
2025-10-01 08:53:48 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WBX());
+}
+
+class WBX extends StatelessWidget {
+  const WBX({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
