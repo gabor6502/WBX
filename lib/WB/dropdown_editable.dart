0ed12edce4111accd2dcba7e3631144f1f27ddb0
gabor6502 <elan.gabor@gmail.com>
2025-10-01 10:42:43 -0500

    adding text areas for editing

--- lib/WB/dropdown_editable.dart
+++ lib/WB/dropdown_editable.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wbx/WB/wb_textfield.dart';
 
 enum Operation { select, edit, add }
 
@@ -15,6 +16,37 @@ class DropdownEditable<T> extends StatefulWidget {
 class _DropdownEditableState<T> extends State<DropdownEditable> {
   Operation currOpp = Operation.select;
 
+  // based on current operation, get an activity for the user to perform
+  getActivity() {
+    if (currOpp == Operation.select) {
+      return DropdownMenu<T>(
+        requestFocusOnTap: true,
+        label: Text(widget.label),
+        dropdownMenuEntries: widget.list.map((elem) {
+          return DropdownMenuEntry<T>(value: elem, label: elem.name);
+        }).toList(),
+      );
+    } else if (currOpp == Operation.add) {
+      return WbTextfield(
+        key: UniqueKey(),
+        onComplete: () {
+          setState(() {
+            currOpp == Operation.select;
+          });
+        },
+      );
+    } else if (currOpp == Operation.edit) {
+      return WbTextfield(
+        key: UniqueKey(),
+        onComplete: () {
+          setState(() {
+            currOpp == Operation.select;
+          });
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,13 +55,7 @@ class _DropdownEditableState<T> extends State<DropdownEditable> {
       ),
       child: Row(
         children: [
-          DropdownMenu<T>(
-            requestFocusOnTap: true,
-            label: Text(widget.label),
-            dropdownMenuEntries: widget.list.map((elem) {
-              return DropdownMenuEntry<T>(value: elem, label: elem.name);
-            }).toList(),
-          ),
+          getActivity(),
           IconButton(
             onPressed: () {
               setState(() {
